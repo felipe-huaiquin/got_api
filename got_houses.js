ba6b37fedb9d550fac821fef9d96e42831363c5e
Felipe <fhuaiquing@gmail.com>
2020-02-19 18:57:19 -0300

    it shows a CORS error

--- got_houses.js
+++ got_houses.js
@@ -2,7 +2,15 @@ $(document).ready(function(){
     $('.container_house').hide();
     $(this).click(function(){
         var link = "https://anapioficeandfire.com/api/houses/"+$('figure').attr("id");
+        $('.container_house').show();
         console.log(link);
-    $.get()
-    })   
+    // $.get(link, function(res){ //it generates an CORS error
+    //     console.log(res);
+    //     $('.house_names').append(res.name);
+    //     $('house_word').append(res.words);
+    //     for(var i = 0; i < res.titles.lenght; i++){
+    //         $('house_titles').append(res.titles[i]);
+    //     }
+    // }, "json");
+    });   
 });
